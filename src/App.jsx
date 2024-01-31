46603e38d384cb3f05f9e687077a7914d4bf746f
Wahajx05 <ks8791882@gmail.com>
2024-01-31 10:30:53 +0500

    React Cake

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CakeryComponent from './Components/CakeryComponent'
+
+const App = () => {
+  return (
+    <>
+      <CakeryComponent />
+    </>
+  )
+}
+
+export default App
